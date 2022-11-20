62e1c8756e6663a0a8d2e944dafbc234e852adba
hariskurtishi <Hariskay95@gmail.com>
2022-11-20 23:07:21 +0100

    Added Employees component and logic

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Search, Page,  Inject, Toolbar } from '@syncfusion/ej2-react-grids'
 
-const Employee = () => {
+import { employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
+
+const Employees  = () => {
   return (
-    <div>Employee</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+      >
+      <ColumnsDirective>
+        {employeesGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective> 
+      <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employee
+export default Employees 

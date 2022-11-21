5b9275da6e85afae9a24e984bf4c3fc7789e9683
hariskurtishi <Hariskay95@gmail.com>
2022-11-21 03:44:44 +0100

    Almost completed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import {
-  SparklineComponent,
-  Inject,
-  SparklineTooltip,
-} from "@syncfusion/ej2-react-charts";
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-class Sparkline extends React.PureComponent {
+class SparkLine extends React.PureComponent {
   render() {
     const { id, height, width, color, data, type, currentColor } = this.props;
 
@@ -21,12 +17,12 @@ class Sparkline extends React.PureComponent {
         tooltipSettings={{
           visible: true,
           // eslint-disable-next-line no-template-curly-in-string
-          format: "${x} : data ${yval}",
+          format: '${x} : data ${yval}',
           trackLineSettings: {
             visible: true,
           },
         }}
-        markerSettings={{ visible: ["All"], size: 2.5, fill: currentColor }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
         dataSource={data}
         xName="x"
         yName="yval"
@@ -38,4 +34,4 @@ class Sparkline extends React.PureComponent {
   }
 }
 
-export default Sparkline;
+export default SparkLine;

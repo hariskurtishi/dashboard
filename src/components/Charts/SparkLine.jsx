4b97df30d755fc2133c436e4d91c89125e992e67
hariskurtishi <Hariskay95@gmail.com>
2022-11-21 03:09:19 +0100

    Almost completed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-class SparkLine extends React.PureComponent {
+class Sparkline extends React.PureComponent {
   render() {
     const { id, height, width, color, data, type, currentColor } = this.props;
 
@@ -34,4 +34,4 @@ class SparkLine extends React.PureComponent {
   }
 }
 
-export default SparkLine;
+export default Sparkline;
